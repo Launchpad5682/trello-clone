146e7cd6adfd46732e6867552945f65587aa9709
launchpad5682 <launchpad5682@gmail.com>
2021-10-22 11:55:26 +0530

    auth context updated with authentication functionalities and updating on change

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,13 +1,48 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { auth } from "../auth/firebase";
 
 export const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = (props) => {
-  const value = {};
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  //signup function
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  //login function
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  //sign out function
+  function signout() {
+    setCurrentUser(null);
+    return auth.signOut();
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    // cleanup
+    return () => {
+      unsubscribe();
+      setCurrentUser(null);
+    };
+  }, []);
+
+  const value = { currentUser, signup, login, signout };
 
   return (
-    <AuthContext.Provider value={value}>{props.children}</AuthContext.Provider>
+    <AuthContext.Provider value={value}>
+      {!loading && props.children}
+    </AuthContext.Provider>
   );
 };
