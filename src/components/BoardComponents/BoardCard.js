03b100871bce8bbb1af6db985e2b3ab7260a085d
launchpad5682 <launchpad5682@gmail.com>
2021-10-23 16:47:05 +0530

    Board Card initiated

---
+++ src/components/BoardComponents/BoardCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function BoardCard() {
+
+    return (
+        <div className="w-80 h-44 bg-blue-900">
+            
+        </div>
+    )
+}
+
+export default BoardCard
