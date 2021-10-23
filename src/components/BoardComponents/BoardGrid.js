ff9ec2eb7cda73fddd3baf5f689375f93aae40af
launchpad5682 <launchpad5682@gmail.com>
2021-10-23 12:44:58 +0530

    Board Grid component initiated

---
+++ src/components/BoardComponents/BoardGrid.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function BoardGrid() {
+  return (
+    <div className="h-5/6 w-full bg-gray-800 flex flex-col items-center py-8">
+      <div className="w-10/12 h-full bg-gray-900 rounded-2xl border-solid border-gray-500 border-2 shadow-2xl">
+          
+      </div>
+    </div>
+  );
+}
+
+export default BoardGrid;
