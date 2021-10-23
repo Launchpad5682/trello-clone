2b4871b60f0439bc3ff3efa2551b52f7fcf73cad
launchpad5682 <launchpad5682@gmail.com>
2021-10-23 20:05:45 +0530

    BoardLists and list component initiated

---
+++ src/components/BoardComponents/BoardListComponents/BoardLists.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function BoardLists() {
+  return (
+    <div className="h-5/6 w-full bg-red-800 flex flex-col items-center py-8 text-white"></div>
+  );
+}
+
+export default BoardLists;
