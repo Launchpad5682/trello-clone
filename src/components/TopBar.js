d92ba7cef903fba701e5ddb72a7d0e883af552ea
launchpad5682 <launchpad5682@gmail.com>
2021-10-23 12:18:22 +0530

    Top Bar updated

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function TopBar() {
-  return <div className="w-screen h-16 bg-gray-900 shadow-xl text-white">
+  return <div className="w-full h-16 bg-gray-900 shadow-xl text-white">
     In Progress
   </div>;
 }
