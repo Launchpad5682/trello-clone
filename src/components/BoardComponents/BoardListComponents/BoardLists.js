7fb0cfaf8d458c66887ad3c1aa1aeb2333cb3d8f
launchpad5682 <launchpad5682@gmail.com>
2021-10-23 23:02:08 +0530

    board lists implemented with horizontal scroll

--- src/components/BoardComponents/BoardListComponents/BoardLists.js
+++ src/components/BoardComponents/BoardListComponents/BoardLists.js
@@ -1,8 +1,18 @@
 import React from "react";
+import BoardList from "./BoardList";
 
 function BoardLists() {
   return (
-    <div className="h-5/6 w-full bg-red-800 flex flex-col items-center py-8 text-white"></div>
+    <div className="h-5/6 w-min-full px-8 py-8 text-white">
+      <div className="h-full w-full flex flex-row gap-x-3 overflow-x-auto">
+        <BoardList />
+        <BoardList />
+        <BoardList />
+        <BoardList />
+        <BoardList />
+        <BoardList />
+      </div>
+    </div>
   );
 }
 
